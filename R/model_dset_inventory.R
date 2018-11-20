5d692594ce0ad9f893ace7ba94741c6408444b4c
Jeremy J Yang <jeremyjyang@gmail.com>
2018-11-20 10:43:06 -0700

    Memory hog.

--- R/model_dset_inventory.R
+++ R/model_dset_inventory.R
@@ -2,23 +2,35 @@
 ###
 # Inventory the ML-ready datasets, each specific to either OMIM ID or MP ID.
 ###
+#
 library(data.table)
+
+t0 <- proc.time()
 
 ddir <- "data/input"
 
-dfiles <- list.files(ddir, pattern="^[0-9]+.rds")
-message(sprintf("Dataset files: %d", length(dfiles)))
+dfiles <- list.files(ddir, pattern="MP.*\\.rds")
+writeLines(sprintf("Dataset files: %d", length(dfiles)))
 
 i <- 0
 for (f in dfiles) {
   i <- i + 1
   dt <- readRDS(sprintf("%s/%s", ddir, f))
+  setDT(dt)
   writeLines(sprintf("%d. %s:", i, f))
   writeLines(sprintf("\trows: %d", nrow(dt)))
   writeLines(sprintf("\tcols: %d", ncol(dt)))
   attrs <- attributes(dt)
-  for (key in c("Date", "mim_id", "omim_title")) {
-    writeLines(sprintf("\t%s: %s", key, as.character(attrs[[key]])))
+  for (key in names(attrs)) {
+    if (length(attrs[[key]])==1) {
+      writeLines(sprintf("\t%s: %s", key, as.character(attrs[[key]])))
+    } else {
+      writeLines(sprintf("\t%s: length = %d", key, length(attrs[[key]])))
+    }
   }
+  rm(dt)
+  break
 }
+
+print(sprintf("elapsed time (total): %.2fs",(proc.time()-t0)[3]))
 
