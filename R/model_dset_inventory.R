ce80a70e40ad2fc556de2a214977c05711ecb8fc
Jeremy J Yang <jeremyjyang@gmail.com>
2018-11-21 09:31:48 -0700

    JSON out.

--- R/model_dset_inventory.R
+++ R/model_dset_inventory.R
@@ -9,28 +9,26 @@ t0 <- proc.time()
 
 ddir <- "data/input"
 
-dfiles <- list.files(ddir, pattern="MP.*\\.rds")
-writeLines(sprintf("Dataset files: %d", length(dfiles)))
+dfiles <- list.files(ddir, pattern="*\\.rds")
+message(sprintf("Dataset files: %d", length(dfiles)))
 
-i <- 0
 for (f in dfiles) {
-  i <- i + 1
   dt <- readRDS(sprintf("%s/%s", ddir, f))
   setDT(dt)
-  writeLines(sprintf("%d. %s:", i, f))
-  writeLines(sprintf("\trows: %d", nrow(dt)))
-  writeLines(sprintf("\tcols: %d", ncol(dt)))
+  writeLines(sprintf("{\"filename\":\"%s\",", f))
+  writeLines(sprintf("\t\"rows\": %d,", nrow(dt)))
+  writeLines(sprintf("\t\"cols\": %d,", ncol(dt)))
   attrs <- attributes(dt)
   for (key in names(attrs)) {
     if (length(attrs[[key]])==1 & (typeof(attrs[[key]]) %in% c("character","integer","double"))) {
-      writeLines(sprintf("\t%s: %s", key, as.character(attrs[[key]])))
+      writeLines(sprintf("\t\"%s\":\"%s\",", key, as.character(attrs[[key]])))
     } else {
-      writeLines(sprintf("\t%s: length = %d; type = %s", key, length(attrs[[key]]),
-	typeof(attrs[[key]])))
+      writeLines(sprintf("\t\"%s_type\":\"%s\",", key, length(attrs[[key]])))
+      writeLines(sprintf("\t\"%s_length\":%d,", key, length(attrs[[key]])))
     }
   }
   rm(dt)
 }
 
-print(sprintf("elapsed time (total): %.2fs",(proc.time()-t0)[3]))
+message(sprintf("elapsed time (total): %.2fs",(proc.time()-t0)[3]))
 
